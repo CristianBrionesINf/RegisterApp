0b95ce8d8ad21b16daab9c00afe4823c742fbadc
FranciscoSilva <franc.silvaj@duocuc.cl>
2024-11-06 09:15:08 -0300

    Update

--- src/app/lista-alumnos/lista-alumnos.page.ts
+++ src/app/lista-alumnos/lista-alumnos.page.ts
@@ -1,18 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AsistenciaService } from '../asistencia.service';
+import { Asistencia } from '../asistencia.service';
+
 
 @Component({
   selector: 'app-lista-alumnos',
   templateUrl: './lista-alumnos.page.html',
   styleUrls: ['./lista-alumnos.page.scss'],
 })
-export class ListaAlumnosPage {
-  alumnos = this.asistenciaService.obtenerAsistencias('alumno');
+export class ListaAlumnosPage implements OnInit {
+  alumnos: Asistencia[] = [];
+
 
   constructor(private asistenciaService: AsistenciaService) {}
 
-  marcarAsistencia(id: number) {
-    this.asistenciaService.registrarAsistencia({ id, nombre: 'Alumno ' + id, presente: true, tipo: 'alumno' });
+  ngOnInit() {
+    this.cargarAsistencias();
+  }
+
+  cargarAsistencias() {
+  this.asistenciaService.obtenerAsistencias().subscribe((data: Asistencia[]) => {
+    this.alumnos = data.filter(asistencia => asistencia.tipo === 'alumno');
+  });
+}
+
+
+  marcarAsistencia(id: number, nombre: string) {
+    this.asistenciaService
+      .registrarAsistencia({ id, nombre, presente: true, tipo: 'alumno' })
+      .subscribe(() => {
+        this.cargarAsistencias();
+      });
   }
 }
 
